f113ca8c9ab673677c43558e53dae3c29e473723
ijgluck <jgluck54@gmail.com>
2021-06-22 12:24:12 +0100

    first commit

---
+++ First_project/src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+fun main(){
+
+    var mean: Short = 25967
+    var num: Int = 25
+    var distance: Long = 850007895
+    var decimals: Double = 859.68
+    var tallEnough: Boolean = true
+    println(num)
+    println(distance)
+    println(decimals)
+    println(tallEnough)
+//FUNCTIONS WITH/WITHOUT RETURN TYPE AND WITH/WITHOUT ARGUMENT
+fun decimals (first: Double, second: Double): Double{
+    var sum: Double = first + second
+    return sum
+}
+
+fun division (num1: Int, num2: Int): Int{
+    var result: Int = num1 / num2
+    return result
+}
+fun getName (firstName: String, lastName: String): String{
+    return firstName + lastName
+}
+}
+
---
+++ weekThreeAssignment/src/main/kotlin/main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+    var num:Int = 0
+    while (num<100){
+        num ++
+        if (num % 3== 0 && num % 5 == 0){
+            println("FizzBuzz")
+            continue
+        }
+    if (num% 3 == 0){
+        println("Fizz")
+        continue
+    }
+        if (num % 5 == 0 ){
+            println("Buzz")
+            continue
+        }
+    println(num)
+    }
+
+    }
